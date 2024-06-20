45d0a42c5bfa491d4dec4485dc1c223c903a2a4b
TBFMcDuck <trym.mcduck@gmail.com>
2024-06-20 23:10:39 +0200

    Improved mobile device experience

--- script/projects.js
+++ script/projects.js
@@ -1,19 +1,21 @@
 // Dynamic projects cards
-window.addEventListener('resize', adjustColumns);
-window.addEventListener('load', adjustColumns);
+window.addEventListener('resize', adjustColumns); // Adjust columns on resize
+window.addEventListener('load', adjustColumns); // Adjust columns on load
 
 function adjustColumns() {
+    // Optain window width and all columns
     var width = window.innerWidth;
     var cols = document.querySelectorAll('.dynamic-col');
 
-    console.log(width);
-
-    cols.forEach(function(col) {
-        if (width < 1500) { // Adjust as needed
-            col.classList.remove('col-md-2');
-            col.classList.add('col-md-3');''
+    cols.forEach(function(col) { // Iterate over all columns
+        if (width <= 768) { // It's likely a mobile device
+            col.classList.remove('col-md-2', 'col-md-3');
+            col.classList.add('col-12'); // Use 1 card per row on mobile devices
+        } else if (width < 1500) { // Adjust as needed
+            col.classList.remove('col-md-2', 'col-12');
+            col.classList.add('col-md-3');
         } else {
-            col.classList.remove('col-md-3');
+            col.classList.remove('col-md-3', 'col-12');
             col.classList.add('col-md-2');
         }
     });
